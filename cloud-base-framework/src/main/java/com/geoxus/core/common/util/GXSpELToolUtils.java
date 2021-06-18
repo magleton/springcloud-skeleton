6a36d3eadb80204121e2230dc822f07b2b628ccc
britton <britton@126.com>
2021-06-18 14:20:00 +0800

    完善SpEL工具类与升级依赖

--- cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSpELToolUtils.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSpELToolUtils.java
@@ -7,14 +7,18 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
+ * SpEL工具类
+ *
  * @author <email>britton@126.com</email>
  */
 public class GXSpELToolUtils {
     /**
      * 私有函数
+     * 防止被外部构造
      */
     private GXSpELToolUtils() {
 
@@ -25,13 +29,17 @@ public class GXSpELToolUtils {
      *
      * @param data             数据
      * @param expressionString 表达式  #data['name']=='jack' and #data['flags']==true
-     * @return boolean
+     * @param beanClass        返回的数据类型  Dog.class
+     * @param dataKey          数据key data1 那么expressionString #data1['name']=='jack' and #data1['flags']==true
+     * @return T
      */
-    public static boolean calculatorExpression(Map<String, Object> data, String expressionString) {
+    public static <T> T calculateSpELExpression(Map<String, Object> data, String expressionString, Class<T> beanClass, String dataKey) {
         ExpressionParser parser = new SpelExpressionParser();
         EvaluationContext context = new StandardEvaluationContext();
-        context.setVariable("data", data);
+        dataKey = Objects.isNull(dataKey) ? "data" : dataKey;
+        context.setVariable(dataKey, data);
         final Expression expression = parser.parseExpression(expressionString);
-        return Optional.ofNullable(expression.getValue(context, boolean.class)).orElse(true);
+        final T expressionValue = expression.getValue(context, beanClass);
+        return Optional.ofNullable(expressionValue).orElse(GXCommonUtils.getClassDefaultValue(beanClass));
     }
 }
