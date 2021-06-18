96234152c12dfb7f7f3bd6934f753532bf2d98f9
britton <britton@126.com>
2021-06-18 17:53:57 +0800

    完善SpEL工具类

--- cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSpELToolUtils.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSpELToolUtils.java
@@ -1,14 +1,16 @@
 package com.geoxus.core.common.util;
 
 import cn.hutool.core.lang.Dict;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.ReflectUtil;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
+import java.lang.reflect.Method;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * SpEL工具类
@@ -44,8 +46,7 @@ public class GXSpELToolUtils {
         dataKey = Objects.isNull(dataKey) ? "data" : dataKey;
         context.setVariable(dataKey, data);
         final Expression expression = parser.parseExpression(expressionString);
-        final T expressionValue = expression.getValue(context, beanClass);
-        return Optional.ofNullable(expressionValue).orElse(GXCommonUtils.getClassDefaultValue(beanClass));
+        return expression.getValue(context, beanClass);
     }
 
     /**
@@ -74,9 +75,49 @@ public class GXSpELToolUtils {
         final StandardEvaluationContext inventorContext = new StandardEvaluationContext(targetObj);
         final ExpressionParser parser = new SpelExpressionParser();
         if (data.isEmpty()) {
-            return GXCommonUtils.getClassDefaultValue(clazz);
+            return null;
         }
         data.forEach((key, value) -> parser.parseExpression(key).setValue(inventorContext, value));
         return parser.parseExpression(targetKey).getValue(inventorContext, clazz);
+    }
+
+    /**
+     * 动态注册函数并调用它进行计算
+     *
+     * <pre>{@code
+     *   public class StringUtils {
+     *       public static String concat(String str1, String str2, String str3) {
+     *         return "链接字符串 : " + str1 + " ---- " + str2 + " ==== " + str3;
+     *     }
+     *   }
+     *   String retVal = registerFunctionSpELExpression(StringUtils.class, "concat", new Class[]{String.class, String.class, String.class}, String.class, "hello", "britton", "枫叶思源");
+     *   }</pre>
+     *
+     * @param targetClass      提供函数的目标类
+     * @param methodName       目标方法
+     * @param methodParamTypes 方法参数的类型
+     * @param clazz            返回值的类型
+     * @param params           调用参数的类型(实参
+     * @return T
+     */
+    public static <T> T registerFunctionSpELExpression(Class<?> targetClass, String methodName, Class<?>[] methodParamTypes, Class<T> clazz, Object... params) {
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("params", params);
+        final Method method = ReflectUtil.getMethod(targetClass, methodName, methodParamTypes);
+        if (Objects.isNull(method)) {
+            return null;
+        }
+        context.registerFunction(methodName, method);
+        StringBuilder methodParam = new StringBuilder();
+        for (int i = 0; i < methodParamTypes.length; i++) {
+            if (i >= params.length) {
+                methodParam.append("null , ");
+                continue;
+            }
+            methodParam.append("#params[").append(i).append("] , ");
+        }
+        final String format = CharSequenceUtil.format("#{}({})", methodName, CharSequenceUtil.subBefore(methodParam.toString(), ',', true));
+        return parser.parseExpression(format).getValue(context, clazz);
     }
 }
