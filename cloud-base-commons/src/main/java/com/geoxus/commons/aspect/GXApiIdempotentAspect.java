c5308e71a9676ad698145eea551a82bb8e42d00e
britton <britton@126.com>
2021-06-28 14:20:35 +0800

    完善API幂等处理

--- cloud-base-commons/src/main/java/com/geoxus/commons/aspect/GXApiIdempotentAspect.java
+++ cloud-base-commons/src/main/java/com/geoxus/commons/aspect/GXApiIdempotentAspect.java
@@ -1,29 +1,28 @@
 package com.geoxus.commons.aspect;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.lang.Dict;
+import cn.hutool.core.lang.TypeReference;
 import cn.hutool.core.text.CharSequenceUtil;
-import cn.hutool.core.util.CharsetUtil;
-import cn.hutool.extra.servlet.ServletUtil;
-import com.geoxus.core.common.service.GXApiIdempotentService;
-import com.geoxus.core.common.util.GXHttpContextUtils;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import cn.hutool.json.JSONUtil;
+import com.geoxus.commons.annotation.GXApiIdempotentAnnotation;
 import com.geoxus.core.common.util.GXResultUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Objects;
 
 @Aspect
 @Component
 public class GXApiIdempotentAspect {
-    private static final String API_IDEMPOTENT_TOKEN = "api-token";
-
-    @Resource
-    private GXApiIdempotentService apiIdempotentService;
-
     @Pointcut("@annotation(com.geoxus.commons.annotation.GXApiIdempotentAnnotation)")
     public void apiIdempotentPointCut() {
         // 切入点表达式, 只是作为一个入口标示
@@ -31,13 +30,89 @@ public class GXApiIdempotentAspect {
 
     @Around("apiIdempotentPointCut()")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        HttpServletRequest request = GXHttpContextUtils.getHttpServletRequest();
-        assert request != null;
-        final String token = ServletUtil.getHeader(request, API_IDEMPOTENT_TOKEN, CharsetUtil.UTF_8);
-        if (null != token && apiIdempotentService.checkApiIdempotentToken(token)) {
+        final Method method = getMethod(point);
+        final Dict condition = getParameters(point);
+        final Object targetObject = getTargetObject(point);
+        if (Objects.isNull(targetObject) || Objects.isNull(method)) {
+            Dict dict = Dict.create().set("error", CharSequenceUtil.format("验证对象信息不存在信息, 请核对是否配置正确~~~"));
+            return GXResultUtils.error(dict);
+        }
+        final boolean retData = invoke(targetObject, method, condition);
+        if (retData) {
             return point.proceed(point.getArgs());
         }
-        Dict dict = Dict.create().set("error", CharSequenceUtil.format("{} HEADERS NOT EXISTS", API_IDEMPOTENT_TOKEN));
+        Dict dict = Dict.create().set("error", CharSequenceUtil.format("请不要提交的太过于频繁~~~~"));
         return GXResultUtils.error(dict);
+    }
+
+    /**
+     * 将注解配置的字段信息和实际参数进行匹配
+     * 并且形成调用函数的实际参数
+     *
+     * @param point 切点对象
+     * @return 实际参数
+     */
+    private Dict getParameters(ProceedingJoinPoint point) {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method signatureMethod = signature.getMethod();
+        final GXApiIdempotentAnnotation methodAnnotation = signatureMethod.getAnnotation(GXApiIdempotentAnnotation.class);
+        final String[] fieldNames = methodAnnotation.fieldNames();
+        final Object[] args = point.getArgs();
+        final Dict targetDict = JSONUtil.toBean(JSONUtil.toJsonStr(args[0]), Dict.class);
+        final Dict condition = Dict.create();
+        for (String fieldName : fieldNames) {
+            final Object o = targetDict.get(fieldName);
+            condition.set(fieldName, o);
+        }
+        return condition;
+    }
+
+    /**
+     * 从Spring容器中获取实际对用对象
+     * 该对象必须是GXApiIdempotentService类型的对象
+     *
+     * @param point 切点对象
+     * @return 实际调用对象
+     */
+    private Object getTargetObject(ProceedingJoinPoint point) {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method signatureMethod = signature.getMethod();
+        final GXApiIdempotentAnnotation methodAnnotation = signatureMethod.getAnnotation(GXApiIdempotentAnnotation.class);
+        final Class<?> service = methodAnnotation.service();
+        return SpringUtil.getBean(service);
+    }
+
+    /**
+     * 通过反射调用函数
+     *
+     * @param targetObject 目标对象
+     * @param targetMethod 目标方法
+     * @param condition    条件
+     * @return 参数
+     * @throws InvocationTargetException 异常对象
+     * @throws IllegalAccessException    异常对象
+     */
+    private boolean invoke(Object targetObject, Method targetMethod, Object condition) throws InvocationTargetException, IllegalAccessException {
+        final Object invoke = targetMethod.invoke(targetObject, condition);
+        if (Objects.isNull(invoke)) {
+            return Boolean.TRUE;
+        }
+        return Convert.convert(new TypeReference<Boolean>() {
+        }, invoke);
+    }
+
+    /**
+     * 从注解信息中获取实际方法
+     *
+     * @param point 切点对象
+     * @return 被调用方法对象
+     */
+    private Method getMethod(ProceedingJoinPoint point) {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method signatureMethod = signature.getMethod();
+        final GXApiIdempotentAnnotation methodAnnotation = signatureMethod.getAnnotation(GXApiIdempotentAnnotation.class);
+        final Class<?> service = methodAnnotation.service();
+        final String methodName = methodAnnotation.methodName();
+        return ReflectUtil.getMethodByNameIgnoreCase(SpringUtil.getBean(service).getClass(), methodName);
     }
 }
--- cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
@@ -4,18 +4,33 @@ import cn.hutool.core.lang.Dict;
 
 public interface GXApiIdempotentService {
     /**
+     * API幂等的Token
+     */
+    String API_IDEMPOTENT_TOKEN = "api-token";
+
+    /**
      * 创建Api的Token
      *
      * @param param 参数
      * @return String
      */
-    String createApiIdempotentToken(Dict param);
+    default String createApiIdempotentToken(Dict param) {
+        return "";
+    }
 
     /**
-     * 验证Api的Token
+     * 自定义接口幂等验证规则
+     * <pre>
+     * {@code
+     * 1、 true 验证通过, 向下继续执行
+     * 2、 false 验证失败, 不会向下继续执行
+     * }
+     * </pre>
      *
-     * @param token token字符串
-     * @return boolean
+     * @param condition 验证条件
+     * @return 验证是否通过
      */
-    boolean checkApiIdempotentToken(String token);
+    default boolean customApiIdempotentValidate(Dict condition) {
+        return Boolean.TRUE;
+    }
 }
