81709590454fdb5b29ff5c14ba91f14809e3ee7f
britton <britton@126.com>
2021-06-28 17:14:06 +0800

    完善API幂等处理

--- cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
@@ -1,6 +1,9 @@
 package com.geoxus.core.common.service;
 
 import cn.hutool.core.lang.Dict;
+import com.geoxus.core.common.util.GXHttpContextUtils;
+
+import java.util.Set;
 
 public interface GXApiIdempotentService {
     /**
@@ -32,5 +35,23 @@ public interface GXApiIdempotentService {
      */
     default boolean customApiIdempotentValidate(Object... condition) {
         return Boolean.TRUE;
+    }
+    
+    /**
+     * 批量获取header中的值
+     *
+     * @param headerNames 头的名字
+     * @return 指定头名字的键值对
+     */
+    default Dict getValueByHeader(Set<String> headerNames) {
+        final Dict dict = Dict.create();
+        if (headerNames.isEmpty()) {
+            return dict;
+        }
+        headerNames.forEach(key -> {
+            final String header = GXHttpContextUtils.getHeader(key);
+            dict.set(key, header);
+        });
+        return dict;
     }
 }
