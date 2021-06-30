2f371ac0d53e18d4cf746fcc57471fd4066ba7a2
britton <britton@126.com>
2021-06-30 18:30:57 +0800

    完善API幂等处理

--- cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/service/GXApiIdempotentService.java
@@ -1,8 +1,18 @@
 package com.geoxus.core.common.service;
 
 import cn.hutool.core.lang.Dict;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.crypto.digest.MD5;
+import cn.hutool.extra.servlet.ServletUtil;
+import cn.hutool.extra.spring.SpringUtil;
+import cn.hutool.json.JSONUtil;
 import com.geoxus.core.common.util.GXHttpContextUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.Set;
 
 public interface GXApiIdempotentService {
@@ -34,9 +44,23 @@ public interface GXApiIdempotentService {
      * @return 验证是否通过
      */
     default boolean customApiIdempotentValidate(Object... condition) {
+        if (Objects.nonNull(condition) && condition.length > 0) {
+            final ValueOperations<String, String> redis = SpringUtil.getBean(StringRedisTemplate.class).opsForValue();
+            final String s = JSONUtil.toJsonStr(condition);
+            String clientIP = "unknown";
+            final HttpServletRequest request = GXHttpContextUtils.getHttpServletRequest();
+            if (Objects.nonNull(request)) {
+                clientIP = ServletUtil.getClientIP(request);
+            }
+            final String cacheKey = MD5.create().digestHex16(CharSequenceUtil.format("{}{}", clientIP, s).getBytes(StandardCharsets.UTF_8));
+            if (Objects.nonNull(redis.get(cacheKey))) {
+                return false;
+            }
+            redis.set(cacheKey, "exists", 600);
+        }
         return Boolean.TRUE;
     }
-    
+
     /**
      * 批量获取header中的值
      *
