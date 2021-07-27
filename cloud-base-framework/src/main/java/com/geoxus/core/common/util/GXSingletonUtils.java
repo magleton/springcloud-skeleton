f6185ffc5cb1940a65800624c5ad968cbc6016ab
britton <britton@126.com>
2021-07-28 07:22:40 +0800

    移除对ehcache的支持

--- cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSingletonUtils.java
+++ cloud-base-framework/src/main/java/com/geoxus/core/common/util/GXSingletonUtils.java
@@ -5,9 +5,6 @@ import cn.hutool.core.lang.TypeReference;
 import cn.hutool.core.text.CharSequenceUtil;
 import com.github.benmanes.caffeine.cache.*;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
-import org.springframework.core.io.ClassPathResource;
 
 import java.util.Objects;
 
@@ -17,33 +14,6 @@ public class GXSingletonUtils {
      */
     private GXSingletonUtils() {
 
-    }
-
-    /**
-     * 获取EhCacheCacheManager
-     *
-     * @return EhCacheCacheManager
-     */
-    public static EhCacheCacheManager getEhCacheCacheManager() {
-        final String beanName = "a1cddb7233521d";
-        EhCacheCacheManager ehCacheCacheManager = GXSpringContextUtils.getBean(EhCacheCacheManager.class);
-        if (null != ehCacheCacheManager) {
-            return ehCacheCacheManager;
-        }
-        ehCacheCacheManager = GXSpringContextUtils.getBean(beanName, EhCacheCacheManager.class);
-        if (null != ehCacheCacheManager) {
-            return ehCacheCacheManager;
-        }
-        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
-        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-        ehCacheManagerFactoryBean.setShared(true);
-        net.sf.ehcache.CacheManager cacheManager = ehCacheManagerFactoryBean.getObject();
-        if (null == cacheManager) {
-            cacheManager = new net.sf.ehcache.CacheManager();
-        }
-        EhCacheCacheManager manager = new EhCacheCacheManager(cacheManager);
-        GXCommonUtils.registerSingleton(beanName, manager);
-        return manager;
     }
 
     /**
