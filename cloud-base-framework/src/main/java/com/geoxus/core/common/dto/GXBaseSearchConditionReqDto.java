8923070736a23b948d9df2f52de5fc90ca1bac23
britton <britton@126.com>
2021-08-04 11:07:53 +0800

    基础功能完善

--- cloud-base-framework/src/main/java/com/geoxus/core/common/dto/GXBaseSearchConditionReqDto.java
+++
@@ -1,9 +0,0 @@
-package com.geoxus.core.common.dto;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-public class GXBaseSearchConditionReqDto extends GXBaseDto {
-}
